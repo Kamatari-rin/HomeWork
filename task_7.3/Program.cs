f76cfcae602a694a53a9fe64f7fd31c29dc2b45b
user <user@email.ru>
2022-09-14 15:09:35 +0500

    Выполнил Задание №3 к семиару 7

---
+++ task_7.3/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Задача №3
+Random rnd = new Random();
+int[,] array = new int[3, 3];
+
+Console.WriteLine("Задача №3: ");
+Console.WriteLine();
+
+for(int i = 0; i < array.GetLength(0); i++) {
+    
+    for(int j = 0; j < array.GetLength(1); j++) {
+
+        array[i, j] = rnd.Next(10, 100);
+        Console.Write(array[i, j] + "\t");
+        
+    }
+    Console.WriteLine();
+}
+
+double columnAvg(int[,] array, int columnIndex) {
+
+    double columnSum = 0;
+    for(int i = 0; i < array.GetLength(0); i++) {
+
+        columnSum += array[i, columnIndex];
+
+    }
+
+    double columnAvg = columnSum / array.GetLength(0);
+
+    return columnAvg;
+}
+
+Console.WriteLine();
+
+double[] avg = new double[array.GetLength(1)];
+
+for(int i = 0; i < array.GetLength(1); i++){
+
+    avg[i] = Math.Round(columnAvg(array, i), 1);
+
+}
+
+Console.WriteLine(string.Join("\t", avg));
