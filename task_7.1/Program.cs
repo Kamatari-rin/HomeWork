5733405f4ee300941f40e93335211eabadaaa670
user <user@email.ru>
2022-09-14 15:08:42 +0500

    Выполнил Задание №1 к семиару 7

---
+++ task_7.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными (тип double) числами.
+// Задача №1
+Random rnd = new Random();
+double[,] array = new double[10, 10];
+
+Console.WriteLine("Задача №3: ");
+
+for(int i = 0; i < 10; i++) {
+    
+    for(int j = 0; j < 10; j++) {
+
+        array[i, j] = Math.Round(rnd.NextDouble() * (1.003 + 97.13), 2);
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
