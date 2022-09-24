b39fd9f7cf0b93b03cc19b56525d456e859480db
user <user@email.ru>
2022-09-25 00:04:51 +0500

    Дз Задание №1 Семинар 8

---
+++ task_8.1/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача №1
+// Задайте двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Random rnd = new Random();
+int[,] array = ArrayFilling(new int[rnd.Next(3, 3), rnd.Next(3, 3)]);
+int[] arrayStringSum = new int[array.GetLength(0)];
+int arrayStringMin = 1;
+
+
+int[,] ArrayFilling(int[,] array) {
+    
+    for(int i = 0; i < array.GetLength(0); i++) {
+        
+        for(int j = 0; j < array.GetLength(1); j++) {
+
+            array[i, j] = rnd.Next(10, 100);
+            
+        }
+    }
+
+    return array;
+}
+
+int[,] ArrayPrint(int[,] array) {
+    
+    for(int i = 0; i < array.GetLength(0); i++) {
+        
+        for(int j = 0; j < array.GetLength(1); j++) {
+
+            Console.Write(array[i, j] + "\t");
+            
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+int[] ArrayStringSum(int[,] array) {
+
+    for(int i = 0; i < array.GetLength(0); i++) {
+
+        for(int j = 0; j < array.GetLength(1); j++) {
+
+            arrayStringSum[i] += array[i, j];
+
+        }
+
+    }
+    return arrayStringSum;
+}
+
+int ArrayStringMin(int[] array) {
+    
+    int buf = array[0];
+    for(int i = 0; i < array.Length; i++) {
+
+        if(array[i] < buf) {
+
+            buf = array[i];
+            arrayStringMin = i + 1;
+
+        }
+
+    } 
+    
+    return arrayStringMin;
+
+}
+
+ArrayPrint(array);
+ArrayStringSum(array);
+
+Console.WriteLine();
+
+Console.WriteLine(string.Join("\t", arrayStringSum));
+
+Console.WriteLine();
+
+Console.WriteLine($"Строка {ArrayStringMin(arrayStringSum)} является той, в которой сумма всех эллементов наименьшия");
